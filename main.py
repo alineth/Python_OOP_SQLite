b108c1aad81061606610c41a4c4a8616acb6806e
Aline TB <112259487+alineth@users.noreply.github.com>
2022-11-28 21:38:44 +0000

    Última aula atualização código.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 #Importa nosso arquivo Pessoa.py no diretório model
 
 from model.Pessoa import Pessoa 
+from database.Database import Database
+from dao.PessoaDAO import PessoaDAO
+
 
 #Exemplo de uso
 
@@ -10,3 +13,33 @@ print(aline)
 #Quero mostrar só o nome
 
 print(aline.nome)
+
+print("DAQUI PRA FRENTE É ACESSO AO BANCO.")
+
+#Chama o objeto de banco de dados
+
+db = Database()
+
+#Instancia o objeto
+
+pessoaDAO = PessoaDAO(db.conexao, db.cursor)
+
+#Quero carregar uma lista com o que veio do banco de dados
+
+pessoas = pessoaDAO.getAll(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
+
+#Criar um objeto com qualquer ator/atriz/diretor/diretora
+  
+novo = Pessoa(0, "Denzel Washington")
+
+#Olha que simples
+
+novo = pessoaDAO.save(novo)
+
+#ocnsulta o banco de novo
+
+pessoas = pessoaDAO.getAll(orderBy=True)
+for pessoa in pessoas:
+  print(pessoa)
